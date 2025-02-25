6a47727e11af073ba2cccfad0ec82e65ca72ae23
Paolet Torres <paolet.torrestorres@gmail.com>
2025-02-24 21:25:50 -0600

    Tercera evision-de-Código

--- filtro.js
+++ filtro.js
@@ -8,56 +8,66 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+//const li = document.getElementsByName("#lista-de-productos"); 
+document.addEventListener("DOMContentLoaded", function(){ //Se agrega esta instrucción para que JavaScript espere hasta que todo el HTML se haya cargado
+  const li = document.querySelector( ".lista-de-productos" ); //Se agrega . ya que está declarado en CSS como una clase
+  //Se usa .querySelector ya que este llama a los elementos de CSS 
+});
+
 
 for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
+  let d = document.createElement("div")
   d.classList.add("producto")
 
-  var ti = document.createElement("p")
+  let ti = document.createElement("p") //Se cambia var por let 
   ti.classList.add("titulo")
   ti.textContent = productos[i].nombre
   
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
+  let imagen = document.createElement("img");
+  imagen.src = productos[i].img; //Borro setAtribute() para hacerlo más concreto  
+  imagen.alt = productos[i].img; //Agregue .alt porque deacuerdo a la documentación si en dado caso que la imagen no llega a aparecer se puede buscar por el texto alternativo. 
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+  d.appendChild(ti);
+  d.appendChild(imagen); //se agrega ; por buena práctica. 
 
-  li.appendChild(d)
+ if(li){ //agrego la sntencia para evitar que si li no está en el DOM no siga con el ciclo
+  li.appendChild(d);
+ }else{ 
+  console.log("No se encontro el producto"); 
+ }  
 }
 
-displayProductos(productos)
+displayProductos(productos);
 const botonDeFiltro = document.querySelector("button");
+const li = document.querySelector(".lista-de-productos"); 
+const input = document.querySelector("miInput") 
 
 botonDeFiltro.onclick = function() {
   while (li.firstChild) {
     li.removeChild(li.firstChild);
   }
 
-  const texto = $i.value;
+  const texto = input.ariaValueMax.toLowerCase(); //Agrego esta instrucción para que convierta en minúsculas 
   console.log(texto);
   const productosFiltrados = filtrado(productos, texto );
 
   for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
+    let d = document.createElement("div")
     d.classList.add("producto")
   
-    var ti = document.createElement("p")
+    let ti = document.createElement("p")
     ti.classList.add("titulo")
     ti.textContent = productosFiltrados[i].nombre
     
-    var imagen = document.createElement("img");
-    imagen.setAttribute('src', productosFiltrados[i].img);
+    let imagen = document.createElement("img");
+    imagen.src = productosFiltrados[i].img; 
+    imagen.alt = productosFiltrados[i].nombre; // Agregue esta instrucción como en la línea 26
   
-    d.appendChild(ti)
-    d.appendChild(imagen)
-  
-    li.appendChild(d)
+    d.appendChild(ti);
+    d.appendChild(imagen);
+    li.appendChild(d);
   }
-}
 
-const filtrado = (productos = [], texto) => {
-  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
-}  
+}; 
+
+const filtrado = (productos, texto) => productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto)); 
